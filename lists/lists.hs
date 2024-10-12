7ed0dee4093e8640a0ab04a72dfd5ae49b44e4bb
Maghan Das <dasmaghan786@gmail.com>
2024-10-12 19:30:27 +0000

    lists in haskell

---
+++ lists/lists.hs
@@ -0,0 +1,27 @@
+
+-- list in haskell is the collection of elements of the same type!
+-- example
+mylist :: [Int]
+mylist = [1, 2, 3, 4, 5]
+
+-- list of strings
+fruits ::[Strings]
+fruits = ["apple", "banana", "cherry"]
+
+-- range of numbers.
+numbers :: [Int]
+numbers = [1,2..10]
+
+-- basic list operations
+-- Accessing Elements
+-- head: Returns the first element of the list.
+-- tail: Returns the list without its first element.
+-- last: Returns the last element of the list.
+-- init: Returns the list without its last element.
+
+numbers :: Int ->Bool
+numbers x = if x`rem` ==0 then true else false
+
+main = print(numbers(2))
+main :: IO ()
+main = putStrLn "Hello, World!"
